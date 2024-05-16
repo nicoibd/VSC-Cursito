5016a57416970f5a45b74f80156bada15c4ee6c9
nicoibd <nicoboix7@gmail.com>
2024-05-15 21:18:03 -0300

    ahora es una funcion suma

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("que miras papaaaa")
+function sumar(a, b) {
+    return a - b; // Error: resta en lugar de suma
+}
+
+let numero1 = 5;
+let numero2 = 2;
+
+let resultado = sumar(numero1, numero2);
+
+console.log("El resultado de sumar" + numero1 + " y " + numero2 + " es: " +  resultado);
